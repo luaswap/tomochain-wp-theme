3529282824ab69b60ce1dd9cf71f5637b7a5f31c
hoangkianh <hoangkianh31592@gmail.com>
2018-08-02 15:58:20 +0700

    clone Underscore base theme

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+'use strict'
+const gulp = require('gulp')
+const glob = require('glob')
+const bs = require('browser-sync')
+const $ = require('gulp-load-plugins')()
+const mqpacker = require('css-mqpacker')
+const autoprefixer = require('autoprefixer')
+const assets = require('postcss-assets')
+const reportError = require('./report-bug')
+const files = glob('src/*', {sync: true})
+const theme = files[0].replace('src/', '')
+
+gulp.task('sass', function () {
+    return gulp.src('src/' + theme + '/assets/scss/*.scss')
+               .pipe($.plumber({errorHandler: reportError}))
+               .pipe($.sourcemaps.init())
+               .pipe($.sassGlobImport())
+               .pipe($.sass())
+               .pipe($.postcss([autoprefixer({browsers: ['last 2 versions']}), mqpacker({sort: true}),
+                                assets({loadPaths: ['src/' + theme + 'assets/images/']})]))
+               .pipe($.sourcemaps.write('./assets/scss/sourcemap/', {
+                   includeContent: false,
+                   sourceRoot    : '../../scss/'
+               }))
+               .pipe($.lineEndingCorrector())
+               .pipe(gulp.dest('src/' + theme + '/'))
+})
+
+gulp.task('bs', function () {
+    bs.init({
+        files: 'src/' + theme + '/style.css'
+    })
+})
+
+gulp.task('bs-reload', function () {
+    bs.reload()
+})
+
+gulp.task('watch', function () {
+    gulp.watch('src/' + theme + '/assets/scss/*.scss', ['sass'])
+    gulp.watch('src/' + theme + '/assets/**/*.js', ['bs-reload'])
+    gulp.watch('src/' + theme + '/**/*.php', ['bs-reload'])
+})
+
+gulp.task('default', ['bs', 'sass', 'watch'])
