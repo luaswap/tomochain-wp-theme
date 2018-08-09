232346c508002e080f82f1b88d5714af5710a388
hoangkianh <hoangkianh31592@gmail.com>
2018-08-09 18:07:33 +0700

    :construction: mobile menu

--- src/tomochain/functions.php
+++ src/tomochain/functions.php
@@ -57,7 +57,7 @@ if ( ! function_exists( 'tomochain_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'tomochain' ),
+			'primary' => esc_html__( 'Primary', 'tomochain' ),
 		) );
 
 		/*
@@ -134,10 +134,6 @@ add_action( 'widgets_init', 'tomochain_widgets_init' );
  */
 function tomochain_scripts() {
 	wp_enqueue_style( 'tomochain-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'tomochain-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'tomochain-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- src/tomochain/header.php
+++ src/tomochain/header.php
@@ -21,6 +21,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+<div class="site-mobile-menu">
+	<?php
+	wp_nav_menu( array(
+		'theme_location' => 'primary',
+		'menu_id'        => 'site-mobile-menu',
+	) );
+	?>
+</div>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tomochain' ); ?></a>
 
@@ -28,33 +36,27 @@
 		<div class="container">
 			<div class="row">
 				<div class="site-branding">
-					<?php
-					the_custom_logo();
-					if ( is_front_page() && is_home() ) :
-						?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php
-					else :
-						?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-						<?php
-					endif;
-					$tomochain_description = get_bloginfo( 'description', 'display' );
-					if ( $tomochain_description || is_customize_preview() ) :
-						?>
-						<p class="site-description"><?php echo $tomochain_description; /* WPCS: xss ok. */ ?></p>
-					<?php endif; ?>
+					<?php the_custom_logo(); ?>
 				</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tomochain' ); ?></button>
+				<nav id="site-navigation" class="main-navigation hidden-lg-down">
 					<?php
 					wp_nav_menu( array(
-						'theme_location' => 'menu-1',
+						'theme_location' => 'primary',
 						'menu_id'        => 'primary-menu',
 					) );
 					?>
 				</nav><!-- #site-navigation -->
+				
+				<div class="header-tools">
+					<div class="mobile-menu-btn hidden-xl-up">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 600">
+							<path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
+							<path d="M300,320 L540,320" id="middle"></path>
+							<path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+						</svg>
+					</div>
+				</div><!-- .header-tools-->
 			</div>
 		</div>
 	</header><!-- #masthead -->
