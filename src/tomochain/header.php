3f1ac8bd739e650f0ce672b220b2f00d2122c710
hoangkianh <hoangkianh31592@gmail.com>
2018-08-13 14:10:22 +0700

    mobile menu btn

--- src/tomochain/header.php
+++ src/tomochain/header.php
@@ -50,13 +50,7 @@
 				</nav><!-- #site-navigation -->
 
 				<div class="header-tools">
-					<div class="mobile-menu-btn hidden-lg-up">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 600">
-							<path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
-							<path d="M300,320 L540,320" id="middle"></path>
-							<path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
-						</svg>
-					</div>
+					<?php tomochain_mobile_menu_btn(); ?>
 				</div><!-- .header-tools-->
 			</div>
 		</div>
