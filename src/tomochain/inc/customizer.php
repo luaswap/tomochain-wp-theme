7fd1e23fa922a2813aaeaa27202b7a016ecf8002
hoangkianh <hoangkianh31592@gmail.com>
2018-08-13 16:14:14 +0700

    remove customizer.js

--- src/tomochain/inc/customizer.php
+++ src/tomochain/inc/customizer.php
@@ -49,11 +49,3 @@ function tomochain_customize_partial_blogname() {
 function tomochain_customize_partial_blogdescription() {
 	bloginfo( 'description' );
 }
-
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
-function tomochain_customize_preview_js() {
-	wp_enqueue_script( 'tomochain-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
-}
-add_action( 'customize_preview_init', 'tomochain_customize_preview_js' );
