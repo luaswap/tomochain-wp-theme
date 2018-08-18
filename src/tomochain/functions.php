996b713c8db446e752d8000bd28de9ee11042c6a
hoangkianh <hoangkianh31592@gmail.com>
2018-08-18 17:09:48 +0700

    complete slider

--- src/tomochain/functions.php
+++ src/tomochain/functions.php
@@ -256,3 +256,18 @@ function my_acf_init() {
     acf_update_setting('google_api_key', get_field('google_maps_api_key', 'options'));
 }
 add_action('acf/init', 'my_acf_init');
+
+/*
+ * Code Snippet to make Revolution Slider enable WPML support when Polylang
+ * is enabled. Polylang has the required WPML compatibility functions to
+ * make Revslider work.
+ *
+ * Add this to your functions.php or using the Code Snippets Plugin
+ *
+ * Author: https://github.com/stuffo/polylang-revslider
+ */
+// only run if Polylang is loaded
+if ( function_exists('pll_languages_list') ) {
+	add_action('wpml_loaded', '__return_true', 10, 0);
+	do_action('wpml_loaded');
+}
