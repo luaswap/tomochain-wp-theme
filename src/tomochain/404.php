5008792be59cf5d2cfefafba401ec13baff969f0
hoangkianh <hoangkianh31592@gmail.com>
2018-09-18 16:17:01 +0700

    change url in 404 page

--- src/tomochain/404.php
+++ src/tomochain/404.php
@@ -8,6 +8,11 @@
  */
 
 get_header();
+$home_url = get_home_url();
+
+if (function_exists('pll_home_url')) {
+    $home_url = pll_home_url();
+}
 ?>
 
 	<div id="primary" class="content-area">
@@ -17,7 +22,7 @@ get_header();
                     <div class="col-12">
                         <img src="<?php echo esc_url(TOMOCHAIN_THEME_URI . '/assets/images/404.png'); ?>" alt="404">
                         <p><?php esc_html_e('Page not found', 'tomochain'); ?></p>
-                        <a href="/" class="tomochain-button"><span><?php esc_html_e('Go Home', 'tomochain') ?></span></a>
+                        <a href="<?php echo esc_url($home_url); ?>" class="tomochain-button"><span><?php esc_html_e('Go Home', 'tomochain') ?></span></a>
                     </div>
                 </div>
             </div>
