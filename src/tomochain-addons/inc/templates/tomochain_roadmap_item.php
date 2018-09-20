f4076c58d8d31b6420ab2ec43406424ae10fda11
hoangkianh <hoangkianh31592@gmail.com>
2018-09-20 16:07:57 +0700

    custom year string for Chinese

--- src/tomochain-addons/inc/templates/tomochain_roadmap_item.php
+++ src/tomochain-addons/inc/templates/tomochain_roadmap_item.php
@@ -17,13 +17,6 @@ extract( $atts );
 $el_class = $this->getExtraClass( $el_class );
 $current_quarter = ceil(intval(date('m')) / 3);
 
-$quarters = array(
-    1 => esc_html__('Q1', 'tomochain-addons' ),
-    2 => esc_html__('Q2', 'tomochain-addons' ),
-    3 => esc_html__('Q3', 'tomochain-addons' ),
-    4 => esc_html__('Q4', 'tomochain-addons' ),
-);
-
 $css_class = array(
     'tomochain-shortcode',
     'tomochain-roadmap-item',
@@ -37,11 +30,24 @@ $css_class = apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG,
     $this->settings['base'],
     $atts );
 
+$quarters = array(
+    1 => esc_html__('Q1', 'tomochain-addons' ),
+    2 => esc_html__('Q2', 'tomochain-addons' ),
+    3 => esc_html__('Q3', 'tomochain-addons' ),
+    4 => esc_html__('Q4', 'tomochain-addons' ),
+);
+
+$year_str = $quarters[$quarter] . ' / ' . $year;
+
+if (function_exists('pll_current_language') && pll_current_language() == 'cn') {
+    $year_str = $year . '年第' . $quarters[$quarter];
+}
+
 ?>
 <div class="<?php echo esc_attr( trim( $css_class ) ); ?>">
     <div class="roadmap-deco">
         <span class="roadmap-dot"><i>&nbsp;</i></span>
     </div>
-    <h5 class="roadmap-title"><?php echo $quarters[$quarter] . ' / ' . $year; ?></h5>
+    <h5 class="roadmap-title"><?php echo wp_kses_post($year_str); ?></h5>
     <div class="roadmap-description"><?php echo '' . $description; ?></div>
 </div>
