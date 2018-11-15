f80e88392ec55d6cef482fa69f757baf66424653
hka <hoangkianh31592@gmail.com>
2018-11-16 06:39:16 +0700

    smoke animation

--- src/tomochain-child/assets/js/script.js
+++ src/tomochain-child/assets/js/script.js
@@ -3,19 +3,19 @@ jQuery(document).ready(function($) {
     var $image = $('#train-image');
 
     $( '#station-1' ).waypoint(function(direction) {
-        $image.css('margin-top', direction == 'down' ? -850 : -1160);
+        $image.css('margin-top', direction == 'down' ? -862 : -1160);
     }, {
         offset: '80%'
     });
 
     $( '#station-2' ).waypoint(function(direction) {
-        $image.css('margin-top', direction == 'down' ? -606 : -850);
+        $image.css('margin-top', direction == 'down' ? -618 : -862);
     }, {
         offset: '80%'
     });
 
     $( '#station-3' ).waypoint(function(direction) {
-        $image.css('margin-top', direction == 'down' ? -362 : -606);
+        $image.css('margin-top', direction == 'down' ? -362 : -618);
     }, {
         offset: '80%'
     });
