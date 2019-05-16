86fc37bc30828232623eb37ec871a82d45015499
Xiem Nguyen <xiem.bg@gmail.com>
2019-05-16 16:46:12 +0700

    merge code bounty

--- src/tomochain/functions.php
+++ src/tomochain/functions.php
@@ -353,7 +353,7 @@ if (defined('SENDGRID_CATEGORY')) {
 // Menu header for page Enterprise
 function register_my_menu() {
   register_nav_menu('header-menu-enterprise',__( 'Header Menu Enterprise' ));
-  register_nav_menu('footer-menu-bounty',__( 'Footer Menu Bounty' ));
+  // register_nav_menu('footer-menu-bounty',__( 'Footer Menu Bounty' ));
 }
 add_action( 'init', 'register_my_menu' );
 
