307d1dd428b4f977d13bccfe27fe8cb12eda8c70
xiemnguyen <xiem.bg@gmail.com>
2019-08-23 09:50:17 +0700

    Create landing page Tomo Stacking

---
+++ src/tomochain-child/tmp-staking.php
@@ -0,0 +1,25 @@
+<?php
+/* Template name: tmp staking */
+
+get_header(); ?>
+<div class="content-area tmp-staking">
+	<main class="site-main">
+		<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
