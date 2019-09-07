040387c6c3d7e58c50b848abcbda50dd9639b4da
xiemnx <xiem.bg@gmail.com>
2019-09-07 23:15:51 +0700

    Create Landingpage TomoX

---
+++ src/tomochain-child/tmp-tomox.php
@@ -0,0 +1,28 @@
+<?php
+/* Template name: tmp tomox */
+
+get_header(); ?>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<div class="content-area tmp-tomox">
+	<main class="site-main">
+		<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
